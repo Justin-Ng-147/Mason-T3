227e76463e6454a0fe0b3845e0f297d163436185
Justin <justin.ng147@gmail.com>
2025-04-28 18:57:58 -0700

    blue 7 changed

--- src/autons/blue7.cpp
+++ src/autons/blue7.cpp
@@ -1,6 +1,6 @@
 #include "main.h"
 
-//new bot blue 4 ring 3 goal
+
 void blue7(){
     intake.set_encoder_units_all(MOTOR_ENCODER_ROTATIONS);
     chassis.setPose(0,0,146);
@@ -9,75 +9,48 @@ void blue7(){
     mogo.set_value(true);
 
     //score ring
-    chassis.moveDistance(6.5,1000);
+    chassis.moveDistance(6,1000);
     arm.move(127);
-    pros::delay(800);
+    pros::delay(500);
     arm.brake();
 
-    chassis.moveToPoint(-15, 31,4000,{.forwards = false,.maxSpeed=80});
-    pros::delay(500);
-    arm_move=false;
-
-    global_target=100;
+    // get mogo
+    chassis.moveToPoint(-10.5, 25.5, 2000, {.forwards=false,.maxSpeed=80,.minSpeed=5,.earlyExitRange=3});
     while(!mogo_seated() && chassis.isInMotion()) pros::delay(10);
     pros::delay(50);
     mogo.set_value(false);
-    chassis.cancelMotion();
-
-
-    // -18 11
-    //get 2 ring red
-    // chassis.turnToPoint(-18,11,2000,{},false);
-    // set_intake_speed(127);
-    // chassis.moveToPoint(-18,11,2000,{.maxSpeed=70});
-
+    arm_move=false;
+    global_target=100;
     set_intake_speed(127);
-    // chassis.moveToPoint(-23,-5,2000,{.minSpeed=5,.earlyExitRange=40});
-    // chassis.moveToPoint(-23,-5,2000,{.maxSpeed=50,.minSpeed=5,.earlyExitRange=3},false);
-    chassis.moveToPoint(12,12,2000,{.minSpeed=5,.earlyExitRange=3});
-    chassis.turnToPoint(-42,30,1000,{.forwards=false,.minSpeed=20,.earlyExitRange=3});
-    chassis.moveDistance(15,1000,{.forwards=false,.minSpeed=20,.earlyExitRange=5});
+
+    chassis.moveToPoint(20,12, 2000, {.minSpeed=5,.earlyExitRange=3});
+    chassis.moveToPoint(-25.7,3, 2000, {.forwards=false, .minSpeed=5,.earlyExitRange=3},false);
+    pros::delay(150);
+    mogo.set_value(true);
+    chassis.moveToPoint(-36,32.7, 2000, {.minSpeed=5,.earlyExitRange=3},false);
+    set_intake_speed(127);
+    pros::Task redg45grj4g4_pos_task{[&]
+        {
+    while(intake_distance.get_distance()>30) pros::delay(5);
+    set_intake_speed(0);
+        }};
+    chassis.moveToPoint(-36.5,51,2000,{.forwards=false, .maxSpeed=60, .minSpeed=5,.earlyExitRange=3},false);
+    if(mogo_seated()){
+        mogo.set_value(false);
+        set_intake_speed(127);
+    }
+    set_intake_speed(127);
+    chassis.moveToPoint(-45,16,1500,{.minSpeed=5,.earlyExitRange=3},false);
+
+    swiper.set_value(true);
+    chassis.moveToPoint(-49.5,2.9,1500,{.minSpeed=5,.earlyExitRange=3}, false);
+    set_intake_speed(0);
+    chassis.turnToHeading(-316.4,1500, {.minSpeed=5,.earlyExitRange=3}, false);
+
     
    
 
-    
-
-    chassis.turnToPoint(-42,30,2000,{.direction=lemlib::AngularDirection::CW_CLOCKWISE,.minSpeed=5,.earlyExitRange=3});
-    // mogo.set_value(false);
-    // set_intake_speed(127);
-    chassis.moveToPoint(-42,30,2000);
-    chassis.moveDistance(10,1000,{.forwards=false},false);
-    mogo.set_value(true);
-    set_intake_speed(-127);
-    chassis.moveDistance(10,1000);
-    
-    
-    chassis.turnToPoint(-44,44,2000,{.forwards=false},false);
-    chassis.moveToPoint(-44,44,4000,{.forwards = false,.maxSpeed=70});
-    while(!mogo_seated() && chassis.isInMotion()) pros::delay(10);
-    pros::delay(50);
-    mogo.set_value(false);
-    chassis.cancelMotion();
-
-
-    fast_move(-38,-4,2000,true);
-    chassis.moveDistance(7,1000,{.forwards=false,.minSpeed=80,.earlyExitRange=3});
-    // fast_move(38,0,2000,true);
-
-    chassis.turnToPoint(-46,-4,1000,{.minSpeed=5,.earlyExitRange=3},false);
-    left.brake();
-    right.brake();
-    set_intake_speed(127);
-    chassis.moveDistance(40,1000);
-    // chassis.moveDistance(10,1000,{.forwards=false,.minSpeed=80,.earlyExitRange=3},false);
-
-    chassis.moveToPoint(-5,41,3000,{.forwards=false});
-    chassis.waitUntil(25);
-    set_intake_speed(0);
-    global_target=6000;
-    while(chassis.isInMotion()) pros::delay(10);
-    arm_move=true;
-    arm.move(0);
+}
 
 
 
@@ -103,4 +76,3 @@ void blue7(){
     // // // chassis.turnToHeading(0,1000);
     // // // chassis.moveDistance(6,1000,{.forwards=false,.minSpeed=30,.earlyExitRange=3});
     // // // chassis.moveDistance(42,10000,{.maxSpeed=30,.minSpeed=30,.earlyExitRange=3},false);
-}
